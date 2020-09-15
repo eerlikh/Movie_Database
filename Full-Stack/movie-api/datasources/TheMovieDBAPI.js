c65a70b2856da390e3165e4dcca589c3ccd91b83
Eduard Erlikh <eerlikh7@gmail.com>
2020-09-15 14:47:26 -0400

    vue project
    made a new vue project

--- Full-Stack/movie-api/datasources/TheMovieDBAPI.js
+++ Full-Stack/movie-api/datasources/TheMovieDBAPI.js
@@ -1,6 +1,5 @@
 const { RESTDataSource } = require('apollo-datasource-rest');
 
-// const https://api.themoviedb.org/3/movie/550?api_key=blooblahbleh
 
 class TheMovieDBAPI extends RESTDataSource {
   constructor() {
