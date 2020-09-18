c33296b969386837a037cb4007cb0dd67454c15c
Eduard Erlikh <eerlikh7@gmail.com>
2020-09-17 20:55:10 -0400

    Added modal for when movie is selected
    Added modal for when movie is selected

--- Full-Stack/frontend-client/src/components/MovieDB.vue
+++ Full-Stack/frontend-client/src/components/MovieDB.vue
@@ -1,15 +1,14 @@
 <template>
   <v-container fluid>
-    <p>{{ selectedMovieId }}</p>
+    <!-- <p>{{ selectedMovieId }}</p> -->
     <v-row>
       <v-col v-for="movie in popularMovies" :key="movie.id" :cols="cols">
-        <!-- <v-card @click="selectedMovieId = movie.id"> -->
         <v-card @click="handleClickMovie(movie.id)">
           <v-img
             :src="`https://image.tmdb.org/t/p/w600_and_h900_bestv2${movie.poster_path}`"
             class="white--text align-end"
             gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,1)"
-            :height="(selectedMovieId === movie.id) ? '700' : '400px'"
+            height="400px"
           >
             <v-card-title v-text="movie.title"></v-card-title>
           </v-img>
@@ -19,6 +18,41 @@
     <!-- <v-card v-for="movie in favoriteMovies" :key="movie.id" class="mb-4">
       {{ movie.title }}
     </v-card> -->
+    <v-dialog v-model="dialog">
+      <v-card v-if="isMovieSelected">
+        <div class="d-flex flex-no-wrap justify-space-between">
+          <v-img 
+            contain
+            :src="`https://image.tmdb.org/t/p/w600_and_h900_bestv2${selectedMovieDetails.poster_path}`"
+          >
+          </v-img>
+          <v-card>
+            <v-card-title>
+              {{ selectedMovieDetails.title }}
+            </v-card-title>
+            <v-card-text>
+              {{ selectedMovieDetails.overview }}
+            </v-card-text>
+          </v-card>
+        </div>
+      </v-card>
+
+      <!-- <v-card>
+        <v-card-title>Select Country</v-card-title>
+        <v-divider></v-divider>
+        <v-card-text style="height: 300px;">
+          <v-radio-group v-model="dialogm1" column>
+            <v-radio label="Burma" value="burma"></v-radio>
+            <v-radio label="Burundi" value="burundi"></v-radio>
+          </v-radio-group>
+        </v-card-text>
+        <v-divider></v-divider>
+        <v-card-actions>
+          <v-btn color="blue darken-1" text @click="dialog = false">Close</v-btn>
+          <v-btn color="blue darken-1" text @click="dialog = false">Save</v-btn>
+        </v-card-actions>
+      </v-card> -->
+    </v-dialog>
   </v-container>
 </template>
 
@@ -35,35 +69,48 @@ export default {
       favoriteMovies: [],
       popularMovies: [],
       selectedMovieId: null,
+      dialog: false
     };
   },
 
   computed: {
     ...mapState(["userId"]),
 
+    selectedMovieDetails() {
+      return this.popularMovies.find(x => x.id === this.selectedMovieId);
+    },
+
+    isMovieSelected() {
+      return !!this.selectedMovieId;
+    },
+
     cols() {
       switch (this.$vuetify.breakpoint.name) {
-        case 'xs':
-          return 12
+        case "xs":
+          return 12;
           break;
-        case 'sm':
-          return 4
+        case "sm":
+          return 4;
           break;
-        case 'md':
-          return 3
+        case "md":
+          return 3;
           break;
         default:
-          return 2
+          return 2;
       }
     }
   },
 
   methods: {
     handleClickMovie(movieId) {
+      // deselect movie
       if (this.selectedMovieId === movieId) {
-        this.selectedMovieId = null
+        this.selectedMovieId = null;
+        this.dialog = false;
+        // select movie
       } else {
-        this.selectedMovieId = movieId
+        this.selectedMovieId = movieId;
+        this.dialog = true;
       }
     }
   },
