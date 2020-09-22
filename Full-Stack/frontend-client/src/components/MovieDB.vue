7bed4928835bf61adf939b4be23e5472af758437
OriginalYellow <michael.robert.joseph1@gmail.com>
2020-09-22 12:27:28 -0400

    fix last commit bugs

--- Full-Stack/frontend-client/src/components/MovieDB.vue
+++ Full-Stack/frontend-client/src/components/MovieDB.vue
@@ -22,36 +22,45 @@
     <!-- <v-card v-for="movie in favoriteMovies" :key="movie.id" class="mb-4">
       {{ movie.title }}
     </v-card> -->
-    <v-dialog v-model="dialog" max-width=900>
-      <v-card v-if="isMovieSelected" tile class="d-flex flex-no-wrap justify-space-between">
-          <v-img 
-            max-height=700
-            max-width=450
-            contain
-            :src="`https://image.tmdb.org/t/p/w600_and_h900_bestv2${selectedMovieDetails.poster_path}`"
-          >
-          </v-img>
-          <v-card>
-            <v-card-title>
-              Title: {{ selectedMovieDetails.title }}
-            </v-card-title>
-            <v-card-subtitle>
-              Rating: {{ selectedMovieDetails.vote_average }}/10
-            </v-card-subtitle>
-            <v-card-text>
-              Overview: {{ selectedMovieDetails.overview }}
-            </v-card-text>
-            <v-card-subtitle>
-              Release Date: {{ selectedMovieDetails.release_date }}
-            </v-card-subtitle>
-            <v-card-actions>
-              <v-btn
-                color="orange"
-                @click="handleFavoriteButtonClick(selectedMovieId)"
-              > favorite </v-btn>
-              <!-- @click="addMovieToFavorites(selectedMovieId)" -->
-            </v-card-actions>
-          </v-card>
+    <v-dialog
+      v-model="dialog"
+      max-width=900
+    >
+      <v-card
+        v-if="isMovieSelected"
+        tile
+        class="d-flex flex-no-wrap justify-space-between"
+      >
+        <v-img
+          max-height=700
+          max-width=450
+          contain
+          :src="`https://image.tmdb.org/t/p/w600_and_h900_bestv2${selectedMovieDetails.poster_path}`"
+        >
+        </v-img>
+        <v-card>
+          <v-card-title>
+            Title: {{ selectedMovieDetails.title }}
+          </v-card-title>
+          <v-card-subtitle>
+            Rating: {{ selectedMovieDetails.vote_average }}/10
+          </v-card-subtitle>
+          <v-card-text>
+            Overview: {{ selectedMovieDetails.overview }}
+          </v-card-text>
+          <v-card-subtitle>
+            Release Date: {{ selectedMovieDetails.release_date }}
+          </v-card-subtitle>
+          <v-card-text>
+            <v-btn
+              color="red"
+              dark
+              :outlined="!isSelectedMovieFavorited"
+              @click="handleFavoriteButtonClick(selectedMovieId)"
+            > {{ isSelectedMovieFavorited ? 'favorted' : 'favorite' }} </v-btn>
+            <!-- @click="addMovieToFavorites(selectedMovieId)" -->
+          </v-card-text>
+        </v-card>
       </v-card>
 
       <!-- <v-card>
@@ -108,7 +117,8 @@ export default {
     },
 
     isMovieSelected() {
-      return !!this.selectedMovieId;
+      // return !!this.selectedMovieId;
+      return this.dialog;
     },
 
     cols() {
@@ -130,19 +140,8 @@ export default {
 
   methods: {
     handleClickMovie(movieId) {
-      // deselect movie
-      if (this.selectedMovieId === movieId) {
-        this.selectedMovieId = null;
-        this.dialog = false;
-        // select movie
-      } else {
-        this.selectedMovieId = movieId;
-        this.dialog = true;
-      }
-    },
-
-    removeMovieFromFavorites(movieId) {
-      return null;
+      this.selectedMovieId = movieId;
+      this.dialog = true;
     },
 
     handleFavoriteButtonClick(movieId) {
@@ -205,9 +204,9 @@ export default {
 
             removeIndex(
               data.user_by_pk.favorite_movies,
-              data.user_by_pk.favorite_movies.findIndex(
-                (x) => x.id === removedId
-              )
+              data.user_by_pk.favorite_movies.findIndex(({ movie }) => {
+                return movie.id === removedId;
+              })
             );
 
             store.writeQuery({
